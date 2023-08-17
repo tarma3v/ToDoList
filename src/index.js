4f54f5ab416ccce93c65bbdf49507b707d9c5228
Тармаев Александр Анатольевич <Tarmaev_A@Dept07>
2023-08-17 17:11:00 +0300

    Make it up-to-date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert(1);
+//alert(1);
+
+setTimeout(5000);
+
+let newTask = document.createElement('h2');
+newTask.className = "task";
+newTask.innerHTML = document.getElementById('taskInput');
+
+document.body.append(newTask);
